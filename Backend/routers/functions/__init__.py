698da4225ea6f95ee8ac295b876634e54d32da76
[myamyam] <[sumin030822@daum.net]>
2024-06-04 23:40:26 +0900

    Final

--- Backend/routers/functions/__init__.py
+++ Backend/routers/functions/__init__.py
@@ -1,8 +1,6 @@
 from . import (  # noqa: F401
     song_generator,
     lyric_generator,
-    acrostic_generator,
-    anime_characterize,
     interview_simulator,
     kospi_analyzer,
 )
--- Models/lyric_generator.py
+++ Models/lyric_generator.py
@@ -2,32 +2,24 @@ from typing import Literal
 
 from pydantic import BaseModel, Field
 
-MAX_LENGTH = 3
-
 class InputModel(BaseModel):
     word: str = Field(
         alias='player_name',
         description='응원가에 들어갈 선수 이름을 입력하세요',
         default='김현준',
-        # min_length=MAX_LENGTH,
-        # max_length=MAX_LENGTH,
-        # pattern=rf'^[a-z|가-힣]{{{MAX_LENGTH}}}$',
     )
     team_name: str = Field(
         description='응원가에 들어갈 팀이름을 입력하세요',
         default='삼성',
-        # min_length=MAX_LENGTH,
-        # max_length=MAX_LENGTH,
-        # pattern=rf'^[a-z|가-힣]{{{MAX_LENGTH}}}$',
     )
     play_style: Literal[
         '안타',
         '홈런',
         '달리기',
     ] = Field(
-        default='Random',
+        default='안타',
     )
-    llm_type: Literal['chatgpt', 'huggingface'] = Field(
+    llm_type: Literal['chatgpt'] = Field(
         alias='Large Language Model Type',
         description='사용할 LLM 종류',
         default='chatgpt',
